bd4bf1ebbf27df135083a9afe8be4aeea04c4da9
Progameitor <javigarzas27@gmail.com>
2019-11-22 14:00:49 +0100

    hasta aqui llegue

---
+++ src/rankeo.py
@@ -0,0 +1,47 @@
+def rankeoair(x):
+    if x >=15:
+        return 4
+    if x >=10 and x<15:
+        return 3
+    if x >=5 and x <10 :
+        return 2
+    if x >0 and x < 5:
+        return 1
+    else:
+        return 0
+
+def rankeovegans(x):
+    if x >=35:
+        return 3
+    if x >=30 and x<35:
+        return 2
+    if x >=20 and x <25 :
+        return 1
+    
+    else:
+        return 0
+
+
+def rankeoschool(x):
+    if x >=45:
+        return 5
+    if x >=35 and x<45:
+        return 3
+    if x >=25 and x <35 :
+        return 2
+    if x >15 and x < 25:
+        return 1
+    else:
+        return 0
+
+def rankeoair(x):
+    if x >=15:
+        return 4
+    if x >=10 and x<15:
+        return 3
+    if x >=5 and x <10 :
+        return 2
+    if x >0 and x < 5:
+        return 1
+    else:
+        return 0
